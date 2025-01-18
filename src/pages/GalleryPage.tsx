580da57146125c69b221548c511f01ac49f4cf9d
Apil-Xenxcel <apil@xenxcel.com>
2025-01-18 09:48:34 +0545

    Add images and update gallery

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -1,56 +1,96 @@
 import { useState } from "react";
-import image1 from "../image/Deluxe Suite.jpg"
-import image2 from "../image/Presidential Suite.jpg"
-import image3 from "../image/Standard Room.jpg"
-import image4 from "../image/Hero.png"
-interface Image {
+import image1 from "../image/Gallery/image1.jpg";
+import image2 from "../image/Gallery/image2.jpg";
+import image3 from "../image/Gallery/image3.jpg";
+import image4 from "../image/Gallery/image4.jpeg";
+import image5 from "../image/Gallery/image5.jpeg";
+import image6 from "../image/Gallery/image6.jpeg";
+import image7 from "../image/Gallery/image7.jpeg";
+import image8 from "../image/Gallery/image8.jpeg";
+import image9 from "../image/Gallery/image9.jpg";
+import image10 from "../image/Gallery/image10.jpg";
+import image11 from "../image/Gallery/image11.jpg";
+import image12 from "../image/Gallery/image12.jpg";
+import image13 from "../image/Gallery/image13.jpg";
+import image14 from "../image/Gallery/image14.jpg";
+import image15 from "../image/Gallery/image15.mp4";
+import image16 from "../image/Gallery/image16.jpg";
+
+interface Media {
   id: number;
   url: string;
+  type: "image" | "video";
 }
 
 const GalleryPage = () => {
-  const [selectedImage, setSelectedImage] = useState<Image | null>(null);
+  const [selectedMedia, setSelectedMedia] = useState<Media | null>(null);
 
-  const images: Image[] = [
-    { id: 1, url: image1 },
-    { id: 2, url: image2 },
-    { id: 3, url: image3 },
-    { id: 4, url: image4 },
-    { id: 2, url: image2 },
-    { id: 3, url: image3 },
-    
+  const media: Media[] = [
+    { id: 1, url: image1, type: "image" },
+    { id: 2, url: image2, type: "image" },
+    { id: 3, url: image3, type: "image" },
+    { id: 4, url: image4, type: "image" },
+    { id: 5, url: image5, type: "image" },
+    { id: 6, url: image6, type: "image" },
+    { id: 7, url: image7, type: "image" },
+    { id: 8, url: image8, type: "image" },
+    { id: 9, url: image9, type: "image" },
+    { id: 10, url: image10, type: "image" },
+    { id: 11, url: image11, type: "image" },
+    { id: 12, url: image12, type: "image" },
+    { id: 13, url: image13, type: "image" },
+    { id: 14, url: image14, type: "image" },
+    { id: 15, url: image15, type: "video" }, // Video
+    { id: 16, url: image16, type: "image" },
   ];
 
-  const openPopup = (image: Image) => setSelectedImage(image);
-  const closePopup = () => setSelectedImage(null);
+  const openPopup = (media: Media) => setSelectedMedia(media);
+  const closePopup = () => setSelectedMedia(null);
 
   return (
-    <div className="mx-1 lg:mx-36 h-screen">
+    <div className="mx-1 lg:mx-36 ">
       <h1 className="text-center text-5xl my-4 font-roboto-flex py-8">Image Gallery</h1>
 
-      {/* Image Grid */}
+      {/* Image/Video Grid */}
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
-        {images.map((image) => (
-          <div key={image.id} className="cursor-pointer">
-            <img
-              src={image.url}
-              alt={`Gallery Image ${image.id}`}
-              className="w-full h-auto object-cover rounded-lg"
-              onClick={() => openPopup(image)}
-            />
+        {media.map((item) => (
+          <div key={item.id} className="cursor-pointer">
+            {item.type === "image" ? (
+              <img
+                src={item.url}
+                alt={`Gallery Media ${item.id}`}
+                className="w-full h-auto object-cover rounded-lg"
+                onClick={() => openPopup(item)}
+              />
+            ) : (
+              <video
+                src={item.url}
+                className="w-full h-auto object-cover rounded-lg"
+                onClick={() => openPopup(item)}
+                controls
+              />
+            )}
           </div>
         ))}
       </div>
 
       {/* Popup Modal */}
-      {selectedImage && (
+      {selectedMedia && (
         <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
           <div className="relative">
-            <img
-              src={selectedImage.url}
-              alt={`Selected Image ${selectedImage.id}`}
-              className="max-w-full max-h-screen rounded-lg"
-            />
+            {selectedMedia.type === "image" ? (
+              <img
+                src={selectedMedia.url}
+                alt={`Selected Media ${selectedMedia.id}`}
+                className="max-w-full max-h-screen rounded-lg"
+              />
+            ) : (
+              <video
+                src={selectedMedia.url}
+                className="max-w-full max-h-screen rounded-lg"
+                controls
+              />
+            )}
             <button
               className="absolute top-2 right-2 text-white text-2xl font-bold"
               onClick={closePopup}
--- src/pages/Home/RoomsSection.tsx
+++ src/pages/Home/RoomsSection.tsx
@@ -1,6 +1,6 @@
-import { motion } from 'framer-motion';
-import StandardRoom from '../../image/Standard Room.jpg'
-import DeluxeRoom from '../../image/Deluxe Suite.jpg'
+import { motion } from "framer-motion";
+import OurHouse from "../../image/Accommodations/Accmodarion1.jpg";
+
 const RoomsSection = () => (
   <div className="container mx-auto px-4 py-16">
     <motion.section
@@ -9,11 +9,17 @@ const RoomsSection = () => (
       whileInView={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
     >
-      <h2 className="text-2xl md:text-4xl text-center mb-10 font-roboto-flex">Our Accommodations</h2>
+      <h2 className="text-2xl md:text-4xl text-center mb-10 font-roboto-flex">
+        Our Accommodations
+      </h2>
       <div className="space-y-16">
         {[
-          { id: 1, img: DeluxeRoom, title: 'Deluxe Suite', desc: 'Experience luxury and comfort in our deluxe suite.' },
-           { id: 3, img: StandardRoom, title: 'Standard Room', desc: 'Cozy and well-equipped for a perfect stay.' },
+          {
+            id: 1,
+            img: OurHouse,
+            title: "Traditional Courtyard Guesthouse",
+            desc: "This image captures the serene and rustic charm of a traditional guesthouse centered around an open-air courtyard. The building is designed with two levels of balconies painted in a vibrant blue, which contrasts beautifully with the crisp whitewashed walls. The balconies, supported by sturdy wooden beams, provide access to individual rooms marked by bright blue doors and windows. Stone steps lead down into the courtyard, framed by red wooden railings that add a pop of color to the overall design. The courtyard is open to the sky, allowing natural light to flood the space, creating a bright and airy atmosphere. The roof of the building is constructed with layers of flat stones, adding a unique and practical touch to the architecture, likely to endure local weather conditions. A black water storage tank is placed in the corner, suggesting provisions for essential utilities. The guesthouse exudes a sense of warmth and simplicity, reflecting traditional craftsmanship and offering an inviting space for travelers. This design, blending functionality and cultural authenticity, makes it an ideal retreat for those seeking a peaceful and unique stay.",
+          },
         ].map((room, index) => (
           <motion.div
             key={room.id}
@@ -22,9 +28,13 @@ const RoomsSection = () => (
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
           >
-            <div className={`flex flex-col lg:flex-row ${index % 2 === 0 ? '' : 'lg:flex-row-reverse'}`}>
+            <div
+              className={`flex flex-col lg:flex-row ${
+                index % 2 === 0 ? "" : "lg:flex-row-reverse"
+              }`}
+            >
               {/* Image */}
-              <div className="w-full">
+              <div className="w-full lg:w-1/2">
                 <img
                   src={room.img}
                   alt={room.title}
@@ -32,8 +42,10 @@ const RoomsSection = () => (
                 />
               </div>
               {/* Room Details */}
-              <div className="p-4 flex flex-col justify-center lg:w-1/2">
-                <h3 className="text-xl font-bold mb-2 font-roboto-flex">{room.title}</h3>
+              <div className="w-full lg:w-1/2 p-4 flex flex-col justify-start">
+                <h3 className="text-xl font-bold mb-2 font-roboto-flex">
+                  {room.title}
+                </h3>
                 <p className="text-gray-600 font-lora">{room.desc}</p>
               </div>
             </div>
