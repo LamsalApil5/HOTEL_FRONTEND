33db60097c0715a7b01423ebf9bcc637fe7b9e41
Apil-Xenxcel <apil@xenxcel.com>
2025-01-21 22:09:42 +0545

    update

--- src/pages/Home/HeroSection.tsx
+++ src/pages/Home/HeroSection.tsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
   return (
     <div className="relative flex flex-col lg:flex-row items-center justify-between">
       <div className="w-full lg:w-1/2 flex flex-col items-center justify-center text-center px-4 mb-8 lg:mb-0 order-2 lg:order-1">
-        <div className="relative z-10 mt-0 md:mt-0">
+        <div className="relative z-10 mt-8 md:mt-0">
           <motion.h1
             className="text-5xl lg:text-6xl font-roboto-flex mb-6 text-black"
             initial={{ opacity: 0, y: -20 }}
